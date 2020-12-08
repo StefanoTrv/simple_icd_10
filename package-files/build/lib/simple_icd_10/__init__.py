3950bb975df4c46ee2006d0478c9e47e1d341c75
StefanoTrv <stefanotravasci@gmail.com>
2020-12-08 22:13:47 +0100

    Added folder containing the files used for the packaging Added a folder which contains all the files that were used or created in the process of building the package. This folder should be left alone unless the idea is to create a new release of the package.

---
+++ package-files/build/lib/simple_icd_10/__init__.py
@@ -0,0 +1,1 @@
+from .simple_icd_10 import *
---
+++ package-files/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="simple_icd_10",
+    version="1.0",
+    author="Stefano Travasci",
+    author_email="stefanotravasci@gmail.com",
+    description="A simple python library for ICD-10 codes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/StefanoTrv/simple_icd_10",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
